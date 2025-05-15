f13150f5e313dd4acd9753863f659052b0558b09
daipresents <Dai Fujihara>
2025-05-15 16:16:07 +0900

    vercelで動くように修正を実験

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/qr-reader-app/',
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: '[name].[ext]', // CSSや画像などのアセットを dist 直下に出力
+        entryFileNames: '[name].js',   // エントリーポイントのJSファイル名
+        chunkFileNames: '[name].js',   // チャンクファイルのJSファイル名
+      },
+    },
+  },
+});
